2e87bce55ef7f0a4daeefbbf900205e97b743d9d
Shanto910 <shantoopaul@gmail.com>
2024-12-23 22:12:56 +0600

    main: navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,74 @@
+import { Link, NavLink } from 'react-router-dom';
+
 const Navbar = () => {
 	return (
-		<div>
-			<h1>Navbar</h1>
+		<div className="max-w-7xl mx-auto px-4 lg:px-8">
+			<div className="navbar bg-base-100 px-0">
+				<div className="navbar-start">
+					<div className="dropdown dropdown-start md:hidden">
+						<div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-5 w-5"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor">
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M4 6h16M4 12h16M4 18h7"
+								/>
+							</svg>
+						</div>
+						<ul
+							tabIndex={0}
+							className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+							<li>
+								<NavLink to={'/assignments'}>Assignments</NavLink>
+							</li>
+							{/* <li>
+								<NavLink to={'/pendingAssignments'}>Pending Assignments</NavLink>
+							</li> */}
+						</ul>
+					</div>
+
+					<Link className="btn btn-ghost text-xl">Study Buddies</Link>
+				</div>
+
+				<div className="navbar-end">
+					<div className="hidden md:flex">
+						<NavLink to={'/assignments'} className="btn btn-ghost text-base">
+							Assignments
+						</NavLink>
+						{/* <NavLink to={'/pendingAssignments'} className="btn btn-ghost text-base">
+							Pending Assignments
+						</NavLink> */}
+					</div>
+					{/* <div className="dropdown dropdown-end">
+						<div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+							<div className="w-10 rounded-full">
+								<img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+							</div>
+						</div>
+						<ul
+							tabIndex={0}
+							className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-56 p-2 shadow">
+							<li>
+								<NavLink to={'/createAssignments'}>Create Assignments</NavLink>
+							</li>
+							<li>
+								<NavLink to={'/myAttemptedAssignments'}>
+									My Attempted Assignments
+								</NavLink>
+							</li>
+						</ul>
+					</div>
+					<button className="btn btn-ghost">Log Out</button> */}
+
+					<button className="btn btn-primary">Log In</button>
+				</div>
+			</div>
 		</div>
 	);
 };
